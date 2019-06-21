e96d1b123aeaaa92776b93102b4301bf26095f93
victor <victor@aula-ie.com>
2019-06-21 14:36:16 -0300

    Create a specific page layout

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name':'theme tutorial',
+    'description': 'A description for your theme.',
+    'version':'1.0',
+    'author':'Your name',
+    'data': [
+        'views/layout.xml',
+        'views/pages.xml',
+    ],
+    'category': 'Theme/Creative',
+    'depends': ['website'],
+    'application': True,
+    'active': False,
+    'installable': True,
+}
+
+
